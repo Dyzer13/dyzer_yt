751a1a1d5678cbc2b1d7600130424718c776e4ab
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-05-08 14:00:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,8 +34,8 @@ client.on('message', message => {
     if (message.content === '-Channel') {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
     if(!args[0]) return message.channel.send("no");
-    message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+       message.channel.bulkDelete(args[0]).then(() => {
+       message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
