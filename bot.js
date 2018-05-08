960087f4bd74468bde8b8fd5bca6b94fce38c01b
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-05-08 12:49:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,30 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'hi') {
-    	message.reply('WELCOM IN DYZER_YT');
+    if (message.content === '!hi') {
+    	message.reply('WELCOM IN DISCORD SERVER ***__DYZER_YT__***');
+  	}
+});
+
+});
+
+client.on('message', message => {
+    if (message.content === '!Rules') {
+    	message.reply('***__DYZER_YT__***  
+                      1- No Spaming 
+                      2- No Give me Rank
+                      3- No Give me a Account Minecraft
+                      4- No Send any Discord server here 
+                      5- No Send Any Video in Chat
+                      6- No How You Create Bot or Som');
+  	}
+});
+
+});
+
+client.on('message', message => {
+    if (message.content === '-Channel') {
+    	message.reply('Subscrbie***__DYZER_YTChannel__***:https://www.youtube.com/channel/UC6Zwte0sirqK2oAoy3AaBbA');
   	}
 });
 
