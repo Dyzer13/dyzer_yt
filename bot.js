91980842703164af9c018ea5ba4dccf43152514c
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-05-08 14:12:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,16 +28,7 @@ client.on('message', message => {
     if (message.content === '-Channel') {
     	message.reply('Subscrbie***__DYZER_YTChannel__***:https://www.youtube.com/channel/UC6Zwte0sirqK2oAoy3AaBbA');
   	}
-});
 
-client.on('message', message => {
-    if (message.content === '-Clear') {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-    if(!args[0]) return message.channel.send("no");
-       message.channel.bulkDelete(args[0]).then(() => {
-       message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000)); 
- 	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
