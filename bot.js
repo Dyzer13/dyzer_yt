a395f1680042a732274f02cc5b782987f178bb50
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-05-08 12:27:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,22 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '!id') {
+    	message.reply('  "id": "41771983429993937",
+  "name": "LUL",
+  "roles": [ "41771983429993000", "41771983429993111" ],
+  "user": {
+    "username": "Luigi",
+    "discriminator": "0002",
+    "id": "96008815106887111",
+    "avatar": "5500909a3274e1812beb4e8de6631111"
+  },
+  "require_colons": true,
+  "managed": false,
+  "animated": false');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
