c61d19f6b0d11b82620dc088b3eaa023dbce8af6
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-05-08 13:38:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,19 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+  if(!args[0]) return message.channel.send("no");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+});
+
+}
+
+module.exports.help = {
+  name: "clear"
+}
 
 client.on('message', message => {
     if (message.content === 'ping') {
