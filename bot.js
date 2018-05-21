8a551885faaa8f13d5baa55ed310d0a238e0aba3
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-05-21 13:05:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,3 +125,25 @@ client.on('message', message => {
     }
 });
 
+client.on('message', msg => { 
+if (msg.content.startsWith(`%report`)) {
+// تعريف الارجس
+   let args = msg.content.split(" ").slice(1);
+// لو ما منشن احد يرد عيله
+  if (!msg.mentions.members.first()) return msg.reply(`يجب عليك منشن شخص`)
+// لو ما كتب تبليغ بيقوله اكتب تبليغ
+  if (!args[1]) return msg.reply(`امممم .. اكتب تبليغك`)
+// استبدل <الروم> بأسم الروم حقك
+  if (msg.guild.channels.find('name', '<الروم>')) {
+// استبدل هنا بعد
+    msg.guild.channels.find('name', '<الروم>').send(`
+  تبليغ على : ${msg.mentions.members.first()}
+  بلغ عليه من قبل : ${msg.member}
+  في روم : ${msg.channel.name}
+  السبب : **${args.join(" ").split(msg.mentions.members.first()).slice(' ')}**
+  `)
+  }
+}
+})
+
+
